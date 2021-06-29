111bb6e6a153487487956859485db4610ee2d6b9
Gabi <g.federico.rodriguez@gmail.com>
2021-06-29 19:43:14 -0300

    button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  border-radius: 25px;
+  min-width: 90px;
+  padding: 1% 2%;
+  background: #fefefe;
+  border: 1px solid #c5c5c5;
+  outline: none;
+  color: #2a2b2c;
+  transition: 0.3s all ease;
+  &:hover {
+    background: #2a2b2c;
+    color: #fefefe;
+  }
+`;
